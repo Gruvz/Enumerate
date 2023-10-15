84a296bdc69a45a638f047a8f17c8b285e6f1dca
Gruvz <Gruvz@live.ru>
2023-10-15 17:45:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using MainEnumerate;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Enumerates
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("1 - Программа Example_1 переводит в разные температыные единицы");
+
+
+            Console.Write("Выбирите номер программы:");
+            int Enter = int.Parse(Console.ReadLine());
+
+
+            switch (Enter)
+            {
+                case 1:
+                    Console.Write("\n \nВведите температару в Цельсиях:");
+                    string EnterString = Console.ReadLine().Replace('.', ',');
+                    double Temp = Convert.ToDouble(EnterString);
+                    var Example_1 = new Example_1("Celsius", Temp);
+                    break;
+
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks/Example_1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainEnumerate
+{
+    /*
+     * Создайте перечисление TemperatureUnits для представления единиц измерения температуры (Celsius, Fahrenheit, Kelvin). 
+     * Напишите метод, который преобразует температуру Цельсия в другие  единицы
+     */
+    internal class Example_1
+    {
+        public enum TemperatureUnits : int
+        {
+            Celsius = 1,
+            Fahrenheit,
+            Kelvin
+        }
+
+        string _Name;
+        double _Temp;
+
+        public Example_1(string _name, double _temp)
+        {
+
+            _Name = _name;
+            _Temp = _temp;
+
+            string[] TemperatureUnitsNames = Enum.GetNames<TemperatureUnits>();
+
+
+            Console.WriteLine($"Система может работать со следующими единицуами изменрения");
+
+            for (int i = 1; i < 3; i++)
+                foreach (string name in TemperatureUnitsNames)
+                {
+                    Console.WriteLine($"{i} - {name}");
+                    i++;
+                }
+
+            Console.WriteLine($"Указанная температура по умолчанию указана в Celsius вы ввели темературу {_Temp}");
+            Console.Write("Введи номер в какой формат вы хотите переоброзовать температуру? : ");
+            int EnterUser = int.Parse(Console.ReadLine());
+
+            switch (EnterUser)
+            {
+                case 1:
+                    Console.WriteLine($"Указанное вами знанчение уже было в Цельсиях {_Temp}");
+                    break;
+                case 2:
+                    double FahrenheitConvert = Math.Round(_Temp * 1.8 + 32, 2);
+                    Console.WriteLine($"Значение указанное в Цельсии {_Temp} в Фаренгейтах будет {FahrenheitConvert}");
+                    break;
+                case 3:
+                    double KelvinConvert = Math.Round(_Temp + 273.15, 2);
+                    Console.WriteLine($"Значение указанное в Цельсии {_Temp} в Кельвинах будет {KelvinConvert}");
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели не верный номер");
+                    break;
+
+            }
+
+
+
+
+
+        }
+    }
+}
+
+
+
